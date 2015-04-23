b7767f9bdf96862a9b8d63cb7589045d53080720
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-23 02:39:28 -0400

    Remove extra Meteor.isClient conditions

--- client/index.js
+++ client/index.js
@@ -1,43 +1,41 @@
-if (Meteor.isClient) {
-  Template.body.helpers({
-    people: function () {
-      return People.find({});
-    }
-  });
+Template.body.helpers({
+  people: function () {
+    return People.find({});
+  }
+});
 
-  Template.body.events({
-    "submit .new-person": function (event) {
-      var commaSeparator = /\s*,\s*/;
+Template.body.events({
+  "submit .new-person": function (event) {
+    var commaSeparator = /\s*,\s*/;
 
-      var name = event.target.name.value;
-      var learning = event.target.learning.value.split(commaSeparator);
-      var teaching = event.target.teaching.value.split(commaSeparator);
+    var name = event.target.name.value;
+    var learning = event.target.learning.value.split(commaSeparator);
+    var teaching = event.target.teaching.value.split(commaSeparator);
 
-      People.insert({
-        name: name,
-        learning: learning,
-        teaching: teaching
-      });
-
-      event.target.name.value = "";
-      $(event.target.learning).clearOptions();
-      $(event.target.teaching).clearOptions();
-
-      return false;
-    }
-  });
-
-  $(document).ready(function () {
-    $('.input-list').selectize({
-      create: function (input) {
-        return {
-          value: input,
-          text: input
-        };
-      },
-      plugins: [
-        'remove_button'
-      ]
+    People.insert({
+      name: name,
+      learning: learning,
+      teaching: teaching
     });
+
+    event.target.name.value = "";
+    $(event.target.learning).clearOptions();
+    $(event.target.teaching).clearOptions();
+
+    return false;
+  }
+});
+
+$(document).ready(function () {
+  $('.input-list').selectize({
+    create: function (input) {
+      return {
+        value: input,
+        text: input
+      };
+    },
+    plugins: [
+      'remove_button'
+    ]
   });
-}
+});
