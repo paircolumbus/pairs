c3e8676adb46b5062830d71dd6b3f8d540eed404
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-24 17:42:25 -0400

    still WIP

--- pairs.js
+++ pairs.js
@@ -1,26 +1,26 @@
 var People = new Meteor.Collection("people");
 var Pairs = new Meteor.Collection("pairs");
-var Cards = new Meteor.Collection("cards");
 
-
+/*
 Pairs.find({}).observeChanges({
   removed: function(id) {
     console.log("In client, Thing removed " + id );
     var id = "a" + id;
     var selector_id = "#" + id;
 
-    var removing_circle = d3.select("#board").selectAll(selector_id);
-    removing_circle.remove();
+    var removing_card = d3.select("#board").selectAll(selector_id);
+    removing_card.remove();
 
-    existing_circles = d3.select("#board").selectAll("g");
-    console.log("new # of things = " + existing_circles.size());
+    existing_cards = d3.select("#board").selectAll("g");
+    console.log("new # of things = " + existing_cards.size());
 
-    x_increment = (500 - 50) / (existing_circles.size() + 1);
+    x_increment = (500 - 50) / (existing_cards.size() + 1);
     x_next = x_increment + 50;
 
-    Template.board._draw_existing(existing_circles, x_next, x_increment);
+    Template.board._draw_existing(existing_cards, x_next, x_increment);
   }
 });
+*/
 
 
 pair = function(list){
@@ -35,6 +35,14 @@ pair = function(list){
   };
 };
 
+negmod = function(n){
+  if(n % 2 === 1){
+    return 1
+  }else{
+    return -1
+  }
+}
+
 if (Meteor.isClient) {
   Template.body.helpers({
     people: function () {
@@ -45,81 +53,120 @@ if (Meteor.isClient) {
     }
   });
 
+  Template.board.cards = function () {
+    pairss = Pairs.find({});
+    return pairss;
+  }
 
-  Template.board.helpers({
-    cards: function () {
-      return Pairs.find({});
-    },
-    card: function() {
+  var drag = d3.behavior.drag()
+      .origin(function(d) { return d; })
+      .on("dragstart", dragstarted)
+      .on("drag", dragged)
+      .on("dragend", dragended);
+
+  function dragstarted(d) {
+    d3.event.sourceEvent.stopPropagation();
+    d3.select(this).classed("dragging", true);
+    console.log("drag started...");
+  }
+
+  function dragged(d) {
+    console.log(d);
+    d3.select(this)
+      .attr("x", d.x = d3.event.x)
+      .attr("y", d.y = d3.event.y);
+
+    console.log(d);
+    console.log("d3.event: "+[d.x, d.y]);
+    console.log("d3.event: "+[d3.event.dx, d3.event.dy]);
+    console.log("d3.event: "+[d3.event.x, d3.event.y]);
+    console.log("d3.event: "+[d.x, d.y]);
+    console.log("dragged ...");
+  }
+
+  function dragended(d) {
+    d3.select(this).classed("dragging", false);
+    console.log("drag ended ...");
+  }
+
+  Template.board.card = function() {
       var id = "a" + this._id;
       var selector_id = "#" + id;
 
-      existing_circles = d3.select("#board").selectAll("g");
-      console.log("current # of things = " + existing_circles.size());
+      existing_cards = d3.select("#board").selectAll("g");
+      //console.log("current # of things = " + existing_cards.size());
 
-      x_increment = (500 - 50) / (existing_circles.size() + 2);
+      x_increment = 210;
       x_next = x_increment + 50;
+      //console.log("x_next is now " + x_next);
+      //console.log("x_increment is now " + x_increment);
 
-      Template.board._draw_existing(existing_circles, x_next, x_increment);
+      y_increment = 100;
+      y_next = y_increment;
 
-      x_next = x_next + (x_increment * existing_circles.size());
+      Template.board._draw_existing(existing_cards, x_next, x_increment, y_next, y_increment);
 
-      circle = d3.select("#board").selectAll(selector_id)
-      circle_data = circle.data([id]);
-      g_container = circle_data.enter()
+      var dd = existing_cards.size();
+      x_next = x_next + ( negmod(dd-1) * (x_increment) ) ;
+      y_next = y_next + ( ( (dd-1) / 2) * (y_increment)) ;
+
+      //console.log("x_next is now " + x_next);
+
+      card = d3.select("#board").selectAll(selector_id)
+      card_data = card.data([id]);
+      g_container = card_data.enter()
         .append("g")
         .classed("thing", true)
         .attr("id", function(d) { return d })
-        .attr("transform", function(d){
+        .attr("transform", function(d,dd){
           i = x_next;
-          x_next = x_next + x_increment;
-          console.log("new circle at x = " + i);
-          return "translate("+i+",100)"
+          j = y_next;
+          x_next = x_next + ( (negmod(dd)) * (x_increment)) ;
+          y_next = y_next + ( (dd % 2) * (y_increment)) ;
+          //console.log("new card at x,y = " + [i,j]);
+          return "translate("+i+","+j+")"
         });
 
         g_container.append("rect")
           .style("stroke", "gray")
           .attr("x", 100)
-          .attr("y", 50)
-          .attr("height", 30)
-          .attr("width", 70)
+          .attr("y", 25)
+          .attr("height", 75)
+          .attr("width", 200)
           .attr("rx", 10)
           .attr("ry", 10)
           .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
-          .on("mouseout", function(){d3.select(this).style("fill", "white");});
-
-
+          .on("mouseout", function(){d3.select(this).style("fill", "white");})
+          .call(drag);
 
         g_container.append("text")
-          .attr("dx", function(d) { return -20 })
+          .attr("dx", function(d) { return 110 })
+          .attr("dy", function(d) { return 75 })
           .text(this.name);
     }
-  });
 
 
-Template.board._draw_existing = function(existing_circles, x_next, x_increment) {
-  existing_circles
-    .transition()
-    .duration(750)
-    .style("stroke", "gray")
-    .attr("transform", function(d){
-      i = x_next;
-      x_next = x_next + x_increment;
-      console.log("move existing circle to x = " + i);
-      return "translate("+i+",100)"
-    });
-}
+  Template.board._draw_existing = function(existing_cards, x_next, x_increment, y_next, y_increment) {
+    existing_cards
+      .transition()
+      .duration(750)
+      .style("stroke", "gray")
+      .attr("transform", function(d, dd){
+        //console.log([d,dd]);
+        i = x_next;
+        j = y_next;
+        x_next = x_next + ( (negmod(dd)) * (x_increment)) ;
+        y_next = y_next + ( (dd % 2) * (y_increment)) ;
+        //console.log("move existing card to x,y = " + [i,j]);
+        return "translate("+i+","+j+")"
+      });
+  }
 
-d3.selection.prototype.size = function() {
-  var n = 0;
-  this.each(function() { ++n; });
-  return n;
-};
-
-
-
-
-
+  d3.selection.prototype.size = function() {
+    var n = 0;
+    this.each(function() { ++n; });
+    return n;
+  };
 
 
   Template.body.events({
@@ -156,12 +203,16 @@ d3.selection.prototype.size = function() {
         'remove_button'
       ]
     });
+
+    Meteor.call('clearPairs');
   });
 
   Template.body.events({
     "submit .pair-it": function (event) {
 
       Meteor.call('clearPairs');
+      d3.selectAll('.thing').remove();
+
 
       pairings = pair(People.find().fetch());
 
@@ -172,11 +223,14 @@ d3.selection.prototype.size = function() {
             pairings.second_half[i]
             ]
         });
+        console.log("shuffle: "+ pairings.first_half[0].name);
       });
+
 
       return false;
     }
   });
+
 }
 
 if (Meteor.isServer) {
