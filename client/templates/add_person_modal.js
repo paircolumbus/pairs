56747b8f88157182eb6ff35e30b4b3dece2dd81c
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-25 19:08:35 -0400

    Save email addresses to the database

--- client/templates/add_person_modal.js
+++ client/templates/add_person_modal.js
@@ -3,16 +3,19 @@ Template.addPersonModal.events({
     var commaSeparator = /\s*,\s*/;
 
     var name = event.target.name.value;
+    var email = event.target.email.value;
     var learning = event.target.learning.value.split(commaSeparator);
     var teaching = event.target.teaching.value.split(commaSeparator);
 
     Meteor.call('insertPerson', {
       name: name,
+      email: email,
       learning: learning,
       teaching: teaching
     });
 
     event.target.name.value = "";
+    event.target.email.value = "";
     $(event.target.learning).clearOptions();
     $(event.target.teaching).clearOptions();
 
