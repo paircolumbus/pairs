bca83bd3c59db7422b0e4e0fbabe6cb2111a5d31
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-25 21:39:00 -0400

    Fix firing events on the pairs template

--- client/templates/pairs.js
+++ client/templates/pairs.js
@@ -3,3 +3,36 @@ Template.pairs.helpers({
     return Pairs.find({});
   }
 });
+
+pair = function(list){
+  var shuffled = _.shuffle(list);
+  var midpoint = Math.floor(shuffled.length / 2);
+  var first_half = shuffled.slice(0,midpoint);
+  var second_half = shuffled.slice(midpoint, shuffled.length);
+
+  return {
+    first_half: first_half,
+    second_half: second_half
+  };
+};
+
+
+Template.pairs.events({
+  "submit .pair-it": function (event) {
+
+    Meteor.call('clearPairs');
+
+    pairings = pair(People.find().fetch());
+
+    pairings.second_half.forEach(function(e,i) {
+      Meteor.call('insertPair', {
+        pair: [
+          pairings.first_half[i],
+          pairings.second_half[i]
+          ]
+      });
+    });
+
+    return false;
+  }
+})
