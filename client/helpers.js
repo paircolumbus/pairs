d91026aa17be463d86676c50addc8d0885dda3a4
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-25 21:44:02 -0400

    embed people in pairs to display their names in pairs

--- client/helpers.js
+++ client/helpers.js
@@ -22,3 +22,9 @@ Template.body.helpers({
 Template.registerHelper("equals", function (a, b) {
   return (a == b);
 });
+
+Template.registerHelper("people_from", function (id_array) {
+  return id_array.map(function(d,i){
+    return People.find({_id: d});
+  });
+});
--- server/methods.js
+++ server/methods.js
@@ -11,6 +11,7 @@ Meteor.methods({
     People.update({_id: id1}, {$set: {pairee: pair_id}});
     People.update({_id: id2}, {$set: {pairee: pair_id}});
 
+    Pairs.update({_id: pair_id}, {$set: {pair: People.find({_id: id1}).fetch().concat(People.find({_id: id2}).fetch())}});
   },
   insertPerson: function (doc) {
     People.insert(doc);
