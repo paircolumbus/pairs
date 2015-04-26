e24e470bfbdc1fb7ff9f6458c2fc8779947ab21b
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-26 11:30:38 -0400

    fix null email error and share more helpers

--- client/helpers.js
+++ client/helpers.js
@@ -7,32 +7,25 @@ Template.registerHelper('unpaired', function () {
 });
 
 Template.registerHelper('avatarFor', function (email) {
-    return Gravatar.imageUrl(email, { size: 32 });
+  if (email == null){
+    email = 'meteor@meteor.com';
+  }
+  return Gravatar.imageUrl(email, { size: 32 });
 });
 
-Template.registerHelper(
-  'pairs', function () {
-    return Pairs.find({});
-  });
+Template.registerHelper( 'pairs', function () {
+  return Pairs.find({});
+});
 
-Template.registerHelper(
-  'numberOfPairs', function () {
-    return Pairs.find({}).count();
-  });
+Template.registerHelper( 'orderedPairs', function () {
+  Meteor.call('orderedPairs');
+  return Pairs.find({});
+});
 
-Template.registerHelper(
-  'numberOfUnpaired', function () {
-    return People.find({ pairee: null }).count();
-  });
+Template.registerHelper( 'numberOfPairs', function () {
+  return Pairs.find({}).count();
+});
 
-Template.registerHelper(
-  'avatarFor', function (email) {
-    return Gravatar.imageUrl(email, { size: 32 });
-  });
-
-Template.registerHelper(
-  'twoPairs', function () {
-    return Gravatar.imageUrl(email, { size: 32 });
-  });
-
-
+Template.registerHelper( 'numberOfUnpaired', function () {
+  return People.find({ pairee: null }).count();
+});
