db584d94aeea3858cd2631d016a9b99670f0dafe
Nicolas McCurdy <thenickperson@gmail.com>
2015-04-25 23:07:20 -0400

    Remove an unused helper

--- client/helpers.js
+++ client/helpers.js
@@ -1,9 +1,3 @@
 Template.registerHelper("equals", function (a, b) {
   return (a == b);
 });
-
-Template.registerHelper("people_from", function (id_array) {
-  return id_array.map(function(d,i){
-    return People.find({_id: d});
-  });
-});
