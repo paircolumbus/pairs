1c271abc03811ca0f930fe632547b8e290fc60d5
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-04-25 22:03:10 -0400

    add a remove button to each person in a pair

--- client/templates/add_person_modal.js
+++ client/templates/add_person_modal.js
@@ -28,6 +28,12 @@ Template.person.events({
     console.log(this.name);
     People.update(this._id, {$set: {pairee: e.target.value}});
     return false;
+  },
+  "submit .remove-one": function (event) {
+    console.log(event);
+    console.log(this);
+    //unpair();
+    return false;
   }
 });
 
