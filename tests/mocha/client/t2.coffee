54d999a7e4cad8807b5bd117b07998424f992e0c
Nicolas McCurdy <thenickperson@gmail.com>
2015-05-03 02:25:05 -0400

    Convert the first test from CoffeeScript to JavaScript

--- tests/mocha/client/t2.coffee
+++
@@ -1,7 +0,0 @@
-MochaWeb?.testOnly ->
-  describe "Pairs view", ->
-    describe "Generate pairs", ->
-      it "pairs the unpaired", ->
-        #create a player
-        chai.assert.equal 5, 5
-        done()
---
+++ tests/mocha/client/t2.js
@@ -0,0 +1,10 @@
+MochaWeb.testOnly(function () {
+  describe("Pairs view", function () {
+    describe("Generate pairs", function () {
+      it("pairs the unpaired", function () {
+        // create a player
+        chai.assert.equal(5, 5);
+      });
+    });
+  });
+});
