dc472181d50c71fb2d4943cf6fd894b1c3d632b9
Nicolas McCurdy <thenickperson@gmail.com>
2015-05-03 02:07:36 -0400

    Remove hard coded columns and use a single Bootstrap row instead

--- client/helpers.js
+++ client/helpers.js
@@ -17,24 +17,6 @@ Template.registerHelper( 'pairs', function () {
   return Pairs.find({});
 });
 
-
-third = function(){
-  return Pairs.find({}).count() / 3;
-}
-twothird = function(){
-  return third() * 2;
-}
-
-Template.registerHelper( 'pairset1', function () {
-  return Pairs.find({},{limit: third()});
-});
-Template.registerHelper( 'pairset2', function () {
-  return Pairs.find({},{skip: third(), limit: third()});
-});
-Template.registerHelper( 'pairset3', function () {
-  return Pairs.find({},{skip: twothird(), limit: third()});
-});
-
 Template.registerHelper( 'numberOfPairs', function () {
   return Pairs.find({}).count();
 });
