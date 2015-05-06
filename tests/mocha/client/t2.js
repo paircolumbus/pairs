574aed74e3ceef4711742c155ab345179be41053
Nicolas McCurdy <thenickperson@gmail.com>
2015-05-06 19:35:28 -0400

    Add pending specs based on requirements in the readme

--- tests/mocha/client/t2.js
+++ tests/mocha/client/t2.js
@@ -9,8 +9,11 @@ MochaWeb.testOnly(function () {
   afterEach(function(){
     //cleanup();
   });
-  describe("Pairs view", function () {
-    describe("Generate pairs", function () {
+
+  describe("pairs page", function () {
+    describe("generating pairs", function () {
+      it("generates pairs randomly, from unpaired people");
+
       it("pairs the unpaired", function (done) {
 
         // create some people
@@ -28,6 +31,62 @@ MochaWeb.testOnly(function () {
           done();
         }, 1500);
       });
+    });
+
+    describe("clearing pairs", function () {
+      it("removes all pairs and unpairs all people");
+    });
+
+    describe("saving pairs", function () {
+      it("save pairs (records who paired with whom)");
+    });
+
+    describe("fullscreen view", function () {
+      it("displays all pairs on one screen");
+    });
+  });
+
+  describe("people page", function () {
+    describe("adding a person", function () {
+      it("creates a new person");
+
+      it("gives the new person an avatar image with Gravatar");
+
+      it("adds arrays of skills that the person is learning and teaching");
+    });
+
+    describe("generating a person", function () {
+      it("creates a new person");
+
+      it("gives the new person a fake name, email, Gravatar image, and skills");
+    });
+
+    describe("deleting a person", function () {
+      context("when the person is unpaired", function () {
+        it("removes the person");
+      });
+
+      context("when the person is paired", function () {
+        it("removes the person");
+
+        it("removes the person's pair");
+
+        it("marks the person's previous pair as unpaired");
+      });
+    });
+  });
+
+  describe("a person", function () {
+    describe("pairing with another person", function () {
+      it("creates a pair with both people");
+
+      it("marks both people as paired with each other");
+    });
+
+    describe("removing from a pair", function () {
+      it("removes the pair");
+
+      it("marks both people as not paired");
     });
   });
 });
