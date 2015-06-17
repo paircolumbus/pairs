4a34b90dccb7d53c1875f51fad3bef16cba64a63
Ryan Kowalick <rkowalick@covermymeds.com>
2015-06-17 19:40:06 -0400

    Access Control for /people

--- routes.js
+++ routes.js
@@ -1,5 +1,16 @@
 Router.configure({ layoutTemplate: 'layout' });
 
 Router.route('/', 'pairs');
-Router.route('/people');
+Router.map(function () {
+  this.route('people', {
+    onBeforeAction: function (pause) {
+      if (!Meteor.user()) {
+        this.render('pairs');
+      } else {
+        this.next();
+      }
+    }
+  });
+});
+
 Router.route('/fullscreen');
