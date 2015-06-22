adbfca202eff4af6c5dbf12bc057a5df4a98ee51
Vasanth Pappu <vasanth.pappu@gmail.com>
2015-06-21 21:08:52 -0400

    this is not dry, how to use common helpers among two templates?

--- client/templates/searchedPerson.js
+++ client/templates/searchedPerson.js
@@ -1,13 +1,9 @@
-Template.person.events({
+Template.searchedPerson.events({
   "click .join-one": function (event) {
-    console.log("joining one");
-    console.log(this._id);
     Meteor.call('joinperson', this._id);
     return false;
   },
   "click .unjoin-one": function (event) {
-    console.log("unjoining one");
-    console.log(this._id);
     Meteor.call('unjoinperson', this._id);
     return false;
   }
