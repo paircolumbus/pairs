903a4c6b40fb2a9605079a3492f6cad3b6e5b4c4
Ciao Chow <ciaochow15@zoho.com>
2015-11-14 15:27:50 -0500

    Add gender/nickname fields

--- client/templates/addPersonModal.js
+++ client/templates/addPersonModal.js
@@ -3,18 +3,24 @@ Template.addPersonModal.events({
     var commaSeparator = /\s*,\s*/;
 
     var name = event.target.name.value;
+    var gender = event.target.gender.value;
+    var nickname = event.target.nickname.value;
     var email = event.target.email.value;
     var learning = event.target.learning.value.split(commaSeparator);
     var teaching = event.target.teaching.value.split(commaSeparator);
 
     Meteor.call('insertPerson', {
       name: name,
+      gender: gender,
+      nickname: nickname,
       email: email,
       learning: learning,
       teaching: teaching
     });
 
     event.target.name.value = "";
+    event.target.gender.value = "";
+    event.target.nickname.value = "";
     event.target.email.value = "";
     $(event.target.learning).clearOptions();
     $(event.target.teaching).clearOptions();
